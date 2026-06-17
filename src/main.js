563c050b63ad825203a86a19a6c53ab60ed3c489
D4NX3L8 <danielochoataborda8@gmail.com>
2026-06-16 22:18:17 -0500

    cree un nuevo encarpetado más ordenado

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { UsuarioModel } from './models/UsuarioModel.js';
-import { UsuarioView } from './views/UsuarioView.js';
-import { UsuarioController } from './controllers/UsuarioController.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const appModel = new UsuarioModel();
-    const appView = new UsuarioView();
-    const appController = new UsuarioController(appModel, appView);
-});
